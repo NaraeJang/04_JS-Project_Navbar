3687436d74133174a50a6e0eb038a73013139ffe
NaraeJang <j.naaaarae@gmail.com>
2023-05-13 12:27:52 -0400

    Complete JS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 04_JS-Project_Navbar
+
+# What I learned?
+
+add - add class
+remove - remove class
+toggle - toggles class
--- app.js
+++ app.js
@@ -3,3 +3,17 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToogle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToogle.addEventListener("click", function () {
+  const linksClass = links.classList;
+  //   if (linksClass.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  //   }
+
+  linksClass.toggle("show-links");
+});
